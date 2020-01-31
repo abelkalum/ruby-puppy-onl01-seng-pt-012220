8c06b0d6baedd938162486fb2d9a0d2d2379140f
Abel Kalum <chumbabel@gmail.com>
2020-01-31 16:28:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,21 @@
-# Add your code here
 class Dog
- 
-  @@all = []
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-     @@all << self
-  end
-  
-  def save
-    @@all << self
-  end
- 
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-  end
-  
-  def clear_all
-    @all.clear
-  end
-  
- end
-  
-end
+
+	@@all = []
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.clear_all
+		@@all.clear
+
+	end
+
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+end 
